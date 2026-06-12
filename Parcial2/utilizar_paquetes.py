135a5a68fcf3f8a727dd1a1d9f377538eff6cd8d
CarlosM-3142250008 <carlos_3142250008@utd.edu.mx>
2026-06-11 23:01:24 -0600

    Update utilizar_paquetes.py

--- Parcial2/utilizar_paquetes.py
+++ Parcial2/utilizar_paquetes.py
@@ -1,9 +1,9 @@
 from paquete1 import modulos,modulo_paquete
 modulos.borrarpantalla()
 modulos.funcion1()
-nom= "Jose"
-ape= "Gomez"
-eda= 36
+nom= "Josue"
+ape= "Sosa"
+eda= 23
 
 name = modulo_paquete.funcion4(nom,ape,eda)
 print(name)
